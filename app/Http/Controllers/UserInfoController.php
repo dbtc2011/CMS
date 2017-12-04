11020687638bbf6207e81e3fc5a93a7c99ebe662
Mark <mark13_06@yahoo.com>
2017-12-05 02:21:55 +0800

    Update

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Storage;
 use App\UserInfo;
 use App\Cv;
 use App\Jv;
+use App\Balance;
 use App\UserCredentials;
 use XBase\Table;
 use Excel;
@@ -52,46 +53,55 @@ class UserInfoController extends Controller
             //'body' => 'required',
         //]);
         // For DBF
-        if ($request->hasFile('dbf')) {
-            $dbfFile = $request->file('dbf');
-            $fileName = $dbfFile->getClientOriginalName();
+        // if ($request->hasFile('dbf')) {
+        //     $dbfFile = $request->file('dbf');
+        //     $fileName = $dbfFile->getClientOriginalName();
 
-            $dbfFile->move('uploads', $fileName);
-            $table = new Table(dirname(base_path()).'/CMS/public/uploads/'.$fileName, array('mem_code', 'acc_code','debit', 'credit', 'chk_no', 'doc_date'));
-            while ($record = $table->nextRecord()) {
-                $account_code = $record->getChar("acc_code");
-                if ($account_code == '40110' || $account_code == '30150' || $account_code == '30140' || $account_code == '30130' || $account_code == '21100001' || $account_code == '11700002' || $account_code == '11700004') {
-                    $newCV = new Jv;
-                    $newCV->mem_code = $record->getChar("mem_code");
-                    $newCV->acc_code = $record->getChar("acc_code");
-                    $newCV->credit = $record->getChar("credit");
-                    $newCV->debit = $record->getChar("debit");
-                    $newCV->chk_no = $record->getChar("chk_no");
-                    $newCV->doc_date = $record->getChar("doc_date");
-                    $newCV->save();
-                }
-            }
-        }
+        //     $dbfFile->move('uploads', $fileName);
+        //     $table = new Table(dirname(base_path()).'/CMS/public/uploads/'.$fileName, array('mem_code', 'acc_code','debit', 'credit', 'chk_no', 'doc_date'));
+        //     while ($record = $table->nextRecord()) {
+        //         $account_code = $record->getChar("acc_code");
+        //         if ($account_code == '40110' || $account_code == '30150' || $account_code == '30140' || $account_code == '30130' || $account_code == '21100001' || $account_code == '11700002' || $account_code == '11700004') {
+        //             $newCV = new Jv;
+        //             $newCV->mem_code = $record->getChar("mem_code");
+        //             $newCV->acc_code = $record->getChar("acc_code");
+        //             $newCV->credit = $record->getChar("credit");
+        //             $newCV->debit = $record->getChar("debit");
+        //             $newCV->chk_no = $record->getChar("chk_no");
+        //             $newCV->doc_date = $record->getChar("doc_date");
+        //             $newCV->save();
+        //         }
+        //     }
+        // }
 
         //For CSV
-        // if ($request->hasFile('csv')) {
+        if ($request->hasFile('csv')) {
 
-        //     $csvFile = $request->file('csv');
-        //     $fileName = $csvFile->getClientOriginalName();
-        //     $csvFile->move('uploads', $fileName);
+            $csvFile = $request->file('csv');
+            $fileName = $csvFile->getClientOriginalName();
+            $csvFile->move('uploads', $fileName);
 
-        //     $path = base_path().'/public/uploads/'.$fileName;
+            $path = base_path().'/public/uploads/'.$fileName;
 
-        //     Excel::load('/public/uploads/'.$fileName, function ($reader) {
-        //         foreach ($reader->toArray() as $key => $row) {
-        //             $newUC = new UserCredentials;
-        //             $newUC->code = $row['code'];
-        //             $newUC->name = $row['name'];
-        //             $newUC->password = $row['password'];
-        //             $newUC->save();
-        //         }
-        //     });
-        // }
+            Excel::load('/public/uploads/'.$fileName, function ($reader) {
+                foreach ($reader->toArray() as $key => $row) {
+                    
+                    // $newUC = new UserCredentials;
+                    // $newUC->code = $row['code'];
+                    // $newUC->name = $row['name'];
+                    // $newUC->password = $row['password'];
+                    // $newUC->save();
+
+                    $new = new Balance;
+                    $new->mem_code = $row['member_codeid'];
+                    $new->acc_code = $row['gl_code'];
+                    $new->title = $row['gl_title'];
+                    $new->amount = $row['amount'];
+                    $new->save();
+                }
+            });
+            exit();
+        }
         return redirect()->route('userInfo.index')
                         ->with('success','User Info created successfully');
     }
